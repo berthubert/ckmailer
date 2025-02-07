ca32d9aed4cec6707f4ad918b64f82eb174cfc2e
bert hubert <bert@hubertnet.nl>
2025-02-07 22:47:43 +0100

    omre

---
+++ html/logic.js
@@ -0,0 +1,47 @@
+"use strict";
+
+async function doRequestAccountPage(f)
+{
+    const formData = new FormData();
+    formData.append('email', f.email);
+
+    const response = await fetch('send-user-account-link', { method: "POST", body: formData });
+    if (response.ok === true) {
+        const data = await response.json();
+	console.log(data);
+	f.state = 1;
+    }
+    else {
+	console.log("error");
+    }
+    return false;
+}
+
+
+async function changeSubscription(timsi, channelid, el)
+{
+    console.log(el.checked);
+    console.log(el);
+    if(el.checked) {
+	console.log("Is checked now!");
+    }
+    else
+	console.log("NOT checked now!");
+ 
+    const formData = new FormData();
+    formData.append('timsi', timsi);
+    formData.append('channelid', channelid);
+    formData.append('to', (el.checked==false) ? 'unsubscribed' : 'subscribed');
+    el.disabled=true;
+    const response = await fetch('change-subscription', { method: "POST", body: formData });
+    if (response.ok === true) {
+        const data = await response.json();
+	console.log(data);
+	el.checked = data["newstate"]=="subscribed";
+	el.disabled=false;
+    }
+    else {
+	console.log("error");
+    }
+    return false;
+}
